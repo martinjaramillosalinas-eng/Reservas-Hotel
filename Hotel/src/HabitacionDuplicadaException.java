6751ae0434b80e45e0dba95c58bdbc7502531e5e
JARA <martinjaramillosalinas@gmail.com>
2026-09-03 07:23:53 -0500

    Update HabitacionDuplicadaException.java

--- Hotel/src/HabitacionDuplicadaException.java
+++ Hotel/src/HabitacionDuplicadaException.java
@@ -1,7 +1,4 @@
-/**
- * Se lanza cuando se intenta registrar dos veces la misma habitacion
- * dentro de una misma reserva.
- */
+
 public class HabitacionDuplicadaException extends Exception {
     public HabitacionDuplicadaException(String mensaje) {
         super(mensaje);
